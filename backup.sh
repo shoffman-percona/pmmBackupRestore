5d3c467963cf81170c5bbb277e143c22cac706d8
Steve Hoffman <53450347+shoffman-percona@users.noreply.github.com>
2022-11-14 19:14:46 -0500

    At least most of the backup portion is working
    There's one thing I'm not sure about ATM with a clickhouse table: schema_migrations. It can't be backed up the same way with the freeze option so waiting to see what that table is even used for and if it even needs to be backed up.

--- backup.sh
+++ backup.sh
@@ -1,36 +1,64 @@
+#!/bin/bash
+
+#vars
+backup_version="pmm_backup_$(date +%s)"
+backup_root="/srv/backups"
+backup_dir=$backup_root/$backup_version
+
+
 #prereqs
+
 yum install -y wget
-victoriametrics --version | cut -d '-' -f7
-wget https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/v1.77.1/vmutils-amd64-v1.77.1.tar.gz
-tar zxvf vmutils-amd64-v1.77.1.tar.gz
+cd /tmp
+vm_version=`victoriametrics --version | cut -d '-' -f7`
+echo "vm version is $vm_version"
+wget https://github.com/VictoriaMetrics/VictoriaMetrics/releases/download/$vm_version/vmutils-amd64-$vm_version.tar.gz
+tar zxvf vmutils-amd64-$vm_version.tar.gz
 
-
+#setup env
+mkdir -p $backup_root/$backup_version/{postgres,vm,clickhouse,folders}
 
 
 #pg backup
-pg_dump -c -U pmm-managed > /tmp/backup.sql
+pg_dump -c -U pmm-managed > $backup_dir/postgres/backup.sql
 
 
 #vm backup
-./vmbackup-prod --storageDataPath=/srv/victoriametrics/data -snapshot.createURL=http://localhost:9090/prometheus/snapshot/create -dst=fs:///srv/backup/vm/
+/tmp/vmbackup-prod --storageDataPath=/srv/victoriametrics/data -snapshot.createURL=http://localhost:9090/prometheus/snapshot/create -dst=fs://$backup_dir/vm/
 
 #clickhouse Backup
-clickhouse-client --host=127.0.0.1 --query "select name from system.tables where database = 'pmm'"
-echo -n 'alter table pmm.metrics freeze' | clickhouse-client --host=127.0.0.1
-clickhouse-client --database "pmm" --query="SHOW CREATE TABLE $table" --format="TabSeparatedRaw" | tee $TARGET_BACKUP_LOCATION/$table.sql
-mkdir /srv/backup/clickhouse
-mv /srv/clickhouse/shadow /srv/backup/clickhouse/backup_name
-#loop through # of tables
+
+mapfile -t ch_array < <(/bin/clickhouse-client --host=127.0.0.1 --query "select name from system.tables where database = 'pmm'")
+for table in "${ch_array[@]}"
+do
+        echo "step 1 on $table"
+        /bin/clickhouse-client --host=127.0.0.1 --query "alter table pmm.$table freeze" 
+        echo "step 2 on $table"
+        /bin/clickhouse-client --host=127.0.0.1 --database "pmm" --query="SHOW CREATE TABLE $table" --format="TabSeparatedRaw" > $backup_dir/clickhouse/$table.sql
+done
+        echo "step 3"
+        mv /srv/clickhouse/shadow $backup_dir/clickhouse/$backup_version
 
 
 #support files backup
-/srv/alerting (root,root)
-/srv/alertmanager (pmm,pmm)
-/srv/grafana (grafana,grafana)
-/srv/ia (root,root)
-/srv/nginx (root,root)
-/srv/prometheus (pmm,pmm)
-/srv/pmm-distribution (root,root) (optional)
+
+cp -af /srv/alerting $backup_dir/folders/
+cp -af /srv/alertmanager $backup_dir/folders/
+cp -af /srv/grafana $backup_dir/folders/
+cp -af /srv/ia $backup_dir/folders/
+cp -af /srv/nginx $backup_dir/folders/
+cp -af /srv/prometheus $backup_dir/folders/
+cp -af /srv/pmm-distribution $backup_dir/folders/
+
+
+
+#$/srv/alerting (root,root)
+#/srv/alertmanager (pmm,pmm)
+#/srv/grafana (grafana,grafana)
+#/srv/ia (root,root)
+#/srv/nginx (root,root)
+#/srv/prometheus (pmm,pmm)
+#/srv/pmm-distribution (root,root) (optional)
 
 
 
@@ -42,20 +70,20 @@ mv /srv/clickhouse/shadow /srv/backup/clickhouse/backup_name
 
 
 #pg restore
-psql -U pmm-managed -f /tmp/backup.sql
+#psql -U pmm-managed -f /tmp/backup.sql
 
 
 #vm restore
-./vmrestore-prod -src=fs:///srv/backup/vm/ -storageDataPath=/srv/victoriametrics/data
+#./vmrestore-prod -src=fs:///srv/backup/vm/ -storageDataPath=/srv/victoriametrics/data
 
 #clickhouse restore
 #will need to loop through #tablenames
-cat $tablename.sql | clickhouse-client --host=127.0.0.1 --database pmm
-mv $SOURCE_BACKUP_LOCATION/data/pmm/$tablename/* /srv/clickhouse/data/pmm/$tablename/detached/
-clickhouse-client --database pmm --query "ALTER TABLE $tablename ATTACH PARTITION 202111"
+#cat $tablename.sql | clickhouse-client --host=127.0.0.1 --database pmm
+#mv $SOURCE_BACKUP_LOCATION/data/pmm/$tablename/* /srv/clickhouse/data/pmm/$tablename/detached/
+#clickhouse-client --database pmm --query "ALTER TABLE $tablename ATTACH PARTITION 202111"
 
 #support files restore
 
 
 #last step
-supervisorctl restart pmm-managed postgresql clickhouse victoriametrics grafana nginx alertmanager qan-api2 vmalert
+#supervisorctl restart pmm-managed postgresql clickhouse victoriametrics grafana nginx alertmanager qan-api2 vmalert
